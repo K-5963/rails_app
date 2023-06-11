e8cb8b8d8bc45c1aaaab54a5f2de79d5015d6280
EC2 Default User <ec2-user@ip-172-31-44-222.ap-northeast-1.compute.internal>
2023-06-11 04:50:50 +0000

    reviewed?メソッド定義

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,10 @@ class Book < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: :user
   
   has_many :reviews
+  
+  def reviewed?(user)
+    Review.find_by(user_id: user.id, book_id: self.id)
+  end
     
   validates :name, presence: true
   validates :description, presence: true
