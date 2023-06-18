1d71005ac0b40854d263d0f045e3c00f56191937
EC2 Default User <ec2-user@ip-172-31-44-222.ap-northeast-1.compute.internal>
2023-06-18 02:32:20 +0000

    starメソッド追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,6 +12,15 @@ class Book < ApplicationRecord
   def average_rate
     reviews.average(:rate).round(1)
   end
+  
+  def star
+    rate = reviews.average(:rate).round(1)
+    return "⭐️" if rate >= 1.0 && rate <= 1.4
+    return "⭐⭐️️" if rate >= 1.5 && rate <= 2.4
+    return "⭐️⭐⭐️" if rate >= 2.5 && rate <= 3.4
+    return "⭐️⭐⭐️⭐️" if rate >= 3.5 && rate <= 4.4
+    return "⭐️⭐⭐️⭐⭐️" if rate >= 4.5 && rate <= 5.0
+  end
     
   validates :name, presence: true
   validates :description, presence: true
